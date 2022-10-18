349085aede680b32e45e9bc17f0d2c6e7c01607c
iammardonbek <100706341+iammardonbek@users.noreply.github.com>
2022-10-18 16:30:51 +0500

    dsfv

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,61 +1,65 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 const LastSalesPage = () => {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState();
+  // const [isLoading, setIsLoading] = useState();
 
-  // const { data, error } = useSWR(
-  //   "https://nextjs-course-67ef1-default-rtdb.firebaseio.com/sales.json"
-  // );
+  const { data, error } = useSWR(
+    "https://nextjs-course-67ef1-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+  console.log(data);
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
   // useEffect(() => {
-  //   if (data) {
-  //     const transformedSales = [];
+  //   setIsLoading(true);
+  //   fetch("https://nextjs-course-67ef1-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
-  //     for (const key in data) {
-  //       transformedSales.push({
-  //         id: key,
-  //         username: data[key].username,
-  //         volume: data[key].volume,
-  //       });
-  //     }
-  //     setSales(transformedSales);
-  //   }
-  // }, [data]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-course-67ef1-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-
-  // if (error) {
-  //   return <p>Failed to load</p>;
-  // }
-  // if (!data || !sales) {
-  //   return <p>...Loading</p>;
-  // }
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+  if (!data && !sales) {
+    return <p>...Loading</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (!sales) {
+  //   return <p>No data yet</p>;
+  // }
+  console.log(sales);
 
   return (
     <ul>
@@ -67,5 +71,28 @@ const LastSalesPage = () => {
     </ul>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-67ef1-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return {
+    props: {
+      sales: transformedSales,
+    },
+    // revalidate: 10,
+  };
+}
 
 export default LastSalesPage;
